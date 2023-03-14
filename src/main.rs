c818352576d705713d69d98342597cf25fbaae34
nakayan5 <tomoya.progress@gmail.com>
2023-03-14 19:21:44 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{http::header::ContentType, App, HttpResponse, HttpServer};
+
+#[actix_web::get("/")]
+async fn hello() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(ContentType::json())
+        .body(r#"{"greet":"Hello, World!"}"#)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 8000))?
+        .run()
+        .await
 }
