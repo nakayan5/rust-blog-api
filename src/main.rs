e6f2f28c652ceab2dfeb683f106278c19b5f5373
nakayan5 <tomoya.progress@gmail.com>
2023-03-15 20:32:55 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-use actix_web::{http::header::ContentType, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
+
+// #[actix_web::get("/")] // アトリビュートマクロ
+// async fn hello() -> HttpResponse {
+//     HttpResponse::Ok()
+//         .append_header(ContentType::json())
+//         .body(r#"{"greet":"Hello, World!"}"#) // r#""#は文字列リテラル記法　エスケープなしに""をかける
+// }
+// curl "http://localhost:8000/"  -> {"greet":"Hello, World!"}
+
+#[derive(Deserialize)]
+struct HelloQuery {
+    name: String,
+    age: u32,
+}
+
+#[derive(Serialize)]
+struct HelloResponse {
+    greet: String,
+}
 
 #[actix_web::get("/")]
-async fn hello() -> HttpResponse {
-    HttpResponse::Ok()
-        .append_header(ContentType::json())
-        .body(r#"{"greet":"Hello, World!"}"#)
+async fn hello(query: web::Query<HelloQuery>) -> HttpResponse {
+    let query = query.into_inner();
+    let message = format!(
+        "Hello, my name is {} I am {} years old.",
+        query.name, query.age
+    );
+
+    let h = HelloResponse { greet: message };
+
+    HttpResponse::Ok().json(h)
 }
+// curl "http://localhost:8000?name=Tom&age=20/"  -> {"greet":"Hello, my name is Tom I am 20 years old."}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,3 +41,5 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+// bindはResult<T, E>の値を返す
+// これに？を適用すると失敗時はEを成功時はTを返してくれる
